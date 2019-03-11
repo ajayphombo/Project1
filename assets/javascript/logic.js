8eefe387900cc96a87c9d18d10b2a35c7a3a3d4a
Mitch <mrmtevans@gmail.com>
2019-03-11 13:47:06 -0700

    added logic for determining opponent id

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,3 +1,4 @@
+// Schedule API
 
 var queryURL = "https://www.thesportsdb.com/api/v1/json/1/eventsnext.php?id=134865"
 $.ajax({
@@ -5,18 +6,34 @@ $.ajax({
     method: "GET",
 
 }).then(function (response) {
-    console.log(response)
-
+    
+console.log(response);
     var results = response.events;
 
     for (var i = 0; i < results.length; i++) {
-        
+
+        //determining the opposing team and againstStatement
+        if (results[i].strHomeTeam === "Golden State Warriors"){
+            var opponent = results[i].strAwayTeam;
+            var againstStatement = "vs " + opponent;
+        }
+        else {
+        var opponent = results[i].strHomeTeam;
+        var againstStatement = "@ " + opponent;
+            };
+
+
+        // displaying the againstStatement and date of game
         var gameDiv = $("<div>");
-        gameDiv.html("<p>" + "Home: " + results[i].strHomeTeam + "</p>" +
-                    "<p>" + "Away: " + results[i].strAwayTeam + "</p>" +
-                    "<p>" + "Date: " + results[i].dateEvent +  "</p>" );
+        gameDiv.html("<p>" + againstStatement + "</p>" +
+                    "<p>" + "Sunday " + results[i].dateEvent + " at " + results[i].strTime + "</p>" );
         gameDiv.addClass("gameContainer");
+        $(gameDiv).attr('id', opponent);
         $("#games-view").append(gameDiv);
-        console.log(gameDiv)
+        console.log(opponent);
     }
+   
 });
+
+
+// GIPHY API
