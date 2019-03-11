5abd89e0494fb0dba5a029b3258d6c731bdd3a08
Mitch <mrmtevans@gmail.com>
2019-03-11 11:19:09 -0700

    schedule api linked to javascript file

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,8 +1,9 @@
 
-var queryURL = "https://www.fantasybasketballnerd.com/service/schedule/GSW"
+var queryURL = "https://www.thesportsdb.com/api/v1/json/1/eventsnext.php?id=134865"
     $.ajax({
         url: queryURL,
-        method: "GET"
+        method: "GET",
+    
     }).then(function (response) {
         console.log(response)
     })
