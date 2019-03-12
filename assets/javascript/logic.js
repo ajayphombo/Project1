4c31dd09123598e39772bf0d394bad56630a42f8
Sy Lim <slimdoe2@gmail.com>
2019-03-11 21:04:15 -0700

    1

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,15 +1,19 @@
 
-var queryURL = "https://www.thesportsdb.com/api/v1/json/1/eventsnext.php?id=134865"
+var queryURL = "https://www.thesportsdb.com/api/v1/json/1/eventsnext.php?id=134865";
+
 $.ajax({
     url: queryURL,
     method: "GET",
 
-}).then(function (response) {
-    console.log(response)
+})
+    .then(function (response) {
+    console.log(response);
 
     var results = response.events;
-
-    for (var i = 0; i < results.length; i++) {
+   
+    console.log(results);
+    for (var i = 0; i < results.length; i++);
+    {
         
         var gameDiv = $("<div>");
         gameDiv.html("<p>" + "Home: " + results[i].strHomeTeam + "</p>" +
